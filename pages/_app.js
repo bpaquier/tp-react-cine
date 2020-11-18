f42ab632228bf8b185b54b69d07bec0059874952
bpaquier <bastien.paquier@gmail.com>
2020-11-18 17:48:36 +0100

    add context for theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,23 @@
+import React, { useState } from 'react';
+
 import '../styles/globals.scss';
+
+import themeContext from '../contexts/theme.tsx';
+import setThemeContext from '../contexts/setTheme.tsx';
 import Header from '../components/Header';
 
 function MyApp({ Component, pageProps }) {
-  return <>
-    <Header />
-    <Component {...pageProps} />
-  </>;
+  const [theme, setTheme] = useState('light');
+  return (
+    <>
+      <themeContext.Provider value={theme}>
+        <setThemeContext.Provider value={setTheme}>
+          <Header />
+          <Component {...pageProps} />
+        </setThemeContext.Provider>
+      </themeContext.Provider>
+    </>
+  );
 }
 
 export default MyApp;
