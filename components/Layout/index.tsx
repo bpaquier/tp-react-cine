9f3f4ec4897a3771d35febf0c9bf4a9cb9291c3c
Quentin GRANCHER <q.grancher@gmail.com>
2020-11-19 16:32:00 +0100

    fix bug layout theme

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,49 +1,46 @@
-import React, { useState } from 'react';
-import Head from 'next/head';
+import React, { useState } from "react";
+import Head from "next/head";
 
-import themeContext from '../../contexts/theme';
-import Header from '../Header';
+import themeContext from "../../contexts/theme";
+import Header from "../Header";
 
 export default function Layout({ ...props }) {
-  const [theme, setTheme] = useState('dark');
+  const [theme, setTheme] = useState("dark");
 
   return (
-    <div>
-      <themeContext.Provider value={[theme, setTheme]}>
+    <themeContext.Provider value={[theme, setTheme]}>
+      <div className={`appContainer ${theme}`}>
         <Head>
-          <title>{props.title || 'ALLO BARBYLONE'}</title>
-          <meta name='title' content='ALLO BARBYLONE' />
+          <title>{props.title || "ALLO BARBYLONE"}</title>
+          <meta name="title" content="ALLO BARBYLONE" />
           <meta
-            name='description'
-            content='AlloBarbylone, the reference website for cinema and tv series! Discover or search for movie ratings, the budget your favorite series etc ...'
+            name="description"
+            content="AlloBarbylone, the reference website for cinema and tv series! Discover or search for movie ratings, the budget your favorite series etc ..."
           />
-
 
           {/* <!-- Open Graph / Facebook --> */}
-          <meta property='og:type' content='website' />
-          <meta property='og:url' content='https://metatags.io/' />
-          <meta property='og:title' content='ALLO BARBYLONE' />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://metatags.io/" />
+          <meta property="og:title" content="ALLO BARBYLONE" />
           <meta
-            property='og:description'
-            content='AlloBarbylone, the reference website for cinema and tv series! Discover or search for movie ratings, the budget your favorite series etc ...'
+            property="og:description"
+            content="AlloBarbylone, the reference website for cinema and tv series! Discover or search for movie ratings, the budget your favorite series etc ..."
           />
-          <meta property='og:image' content='/assets/meta-img.jpg' />
-
+          <meta property="og:image" content="/assets/meta-img.jpg" />
 
           {/* <!-- Twitter --> */}
-          <meta property='twitter:card' content='summary_large_image' />
-          <meta property='twitter:url' content='https://metatags.io/' />
-          <meta property='twitter:title' content='ALLO BARBYLONE' />
+          <meta property="twitter:card" content="summary_large_image" />
+          <meta property="twitter:url" content="https://metatags.io/" />
+          <meta property="twitter:title" content="ALLO BARBYLONE" />
           <meta
-            property='twitter:description'
-            content='AlloBarbylone, the reference website for cinema and tv series! Discover or search for movie ratings, the budget your favorite series etc ...'
+            property="twitter:description"
+            content="AlloBarbylone, the reference website for cinema and tv series! Discover or search for movie ratings, the budget your favorite series etc ..."
           />
-          <meta property='twitter:image' content='/assets/meta-img.jpg'></meta>
+          <meta property="twitter:image" content="/assets/meta-img.jpg"></meta>
         </Head>
         <Header />
         {props.children}
-      </themeContext.Provider>
-    </div>
-
+      </div>
+    </themeContext.Provider>
   );
 }
