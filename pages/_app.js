8d3dfadf5dbd3202f45cda0c8b41a46610d37b74
cpetillot <constance.petillot@hetic.net>
2020-11-19 16:59:46 +0100

    adding footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import '../styles/globals.scss';
+import "../styles/globals.scss";
 
-import themeContext from '../contexts/theme.tsx';
-import setThemeContext from '../contexts/setTheme.tsx';
-import Header from '../components/Header';
+import themeContext from "../contexts/theme.tsx";
+import setThemeContext from "../contexts/setTheme.tsx";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 function MyApp({ Component, pageProps }) {
-  const [theme, setTheme] = useState('dark');
+  const [theme, setTheme] = useState("dark");
   return (
     <>
       <themeContext.Provider value={theme}>
@@ -15,6 +16,7 @@ function MyApp({ Component, pageProps }) {
           <div className={`appContainer ${theme}`}>
             <Header />
             <Component {...pageProps} />
+            <Footer />
           </div>
         </setThemeContext.Provider>
       </themeContext.Provider>
