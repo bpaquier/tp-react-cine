296630bfe948b37d52947859c37931280dc7669a
Nawel Berrichi <nawel.berrichi@hetic.net>
2020-11-19 09:57:59 +0100

    add ratio for card

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import classNames from "classNames/bind";
 import css from "./styles.module.scss";
+import Ratio from "../Ratio";
 const cx = classNames.bind(css);
 const API_URL = "https://api.themoviedb.org/3/search/movie";
 const API_KEY = "a366c741ebcd23ebb98f75ee1b26fece";
@@ -11,7 +12,6 @@ interface CardProps {
 }
 
 const Card = ({ className, movie }: CardProps) => {
-
   const { poster_path, original_title, genres, runtime, release_date } = movie;
   const genresList: string = genres.map((genre) => genre.name).join(" · ");
   function getImageFromApi(name: string): string {
@@ -27,21 +27,30 @@ const Card = ({ className, movie }: CardProps) => {
   }
 
   return (
-    <div className={css.card}>
-      <img className={css.card__picture} src={getImageFromApi(poster_path)} />
-      <div className={css.card__description}>
-        <div className={css.text__container}>
-          <h3>{original_title}</h3>
-          <p>{genresList}</p>
-          <div className={css.text__informations}>
-            <div className={css.text__information}>{minuteToHour(runtime)}</div>
-            <div className={css.text__information}>
-              {release_date.slice(0, 4)}
+    <Ratio ratio={468 / 274}>
+      {(className) => (
+        <div className={css.card}>
+          <img
+            className={css.card__picture}
+            src={getImageFromApi(poster_path)}
+          />
+          <div className={css.card__description}>
+            <div className={css.text__container}>
+              <h3>{original_title}</h3>
+              <p>{genresList}</p>
+              <div className={css.text__informations}>
+                <div className={css.text__information}>
+                  {minuteToHour(runtime)}
+                </div>
+                <div className={css.text__information}>
+                  {release_date.slice(0, 4)}
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </Ratio>
   );
 };
 
---
+++ components/Ratio/index.tsx
@@ -0,0 +1,28 @@
+import React, { DOMAttributes, ReactChild } from "react";
+import classNames from "classnames/bind";
+import css from "./style.module.scss";
+
+const cx = classNames.bind(css);
+
+export interface RatioProps extends DOMAttributes<any> {
+  className?: string;
+  ratio?: number;
+  children: (className: string) => ReactChild;
+}
+
+function Ratio({ ratio, className, children, ...rest }: RatioProps) {
+  return (
+    <div {...rest} className={cx(css.Ratio, className)}>
+      <div
+        className={cx(className, css.ratioContainer)}
+        style={ratio ? { paddingBottom: `${(1 / ratio) * 100}%` } : {}}
+      >
+        {children(css.children)}
+      </div>
+    </div>
+  );
+}
+
+Ratio.defaultProps = {};
+
+export default Ratio;
