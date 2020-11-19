8d73721680d29d50d332ad4f3298c3ef31a9f1ae
Quentin GRANCHER <q.grancher@gmail.com>
2020-11-19 15:46:56 +0100

    add layout component with global theme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,19 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-import { useState } from 'react';
-import Card from '../components/Card';
-import Hero from '../components/Hero';
-import MoviesList from '../components/MovieList';
+import { useState } from "react";
+import Card from "../components/Card";
+import Hero from "../components/Hero";
+import Layout from "../components/Layout";
 
-import searchBarFetch from '../services/searchBarFetch.js';
+import MoviesList from "../components/MovieList";
 
-import css from '../styles/Home.module.css';
+import searchBarFetch from "../services/searchBarFetch.js";
+
+import css from "../styles/Home.module.css";
 
 export default function Home({ data }) {
   const [movies, setMovies] = useState(null);
-  const [userInput, setUserInput] = useState('');
+  const [userInput, setUserInput] = useState("");
 
   async function declencheFetch(query: string) {
     let result = await searchBarFetch(query);
@@ -19,10 +21,10 @@ export default function Home({ data }) {
   }
 
   return (
-    <>
+    <Layout>
       <Hero
-        title='Welcome to Allo Barbylone'
-        subtitle='What are we watching tonight ?'
+        title="Welcome to Allo Barbylone"
+        subtitle="What are we watching tonight ?"
         declencheFetch={declencheFetch}
         userInput={userInput}
         setUserInput={setUserInput}
@@ -32,14 +34,14 @@ export default function Home({ data }) {
         queryMovies={movies}
         popularMovies={data}
       />
-    </>
+    </Layout>
   );
 }
 
 export async function getStaticProps() {
   // Get external data from the file system, API, DB, etc.
   const data = await fetch(
-    'https://api.themoviedb.org/3/movie/popular?api_key=a366c741ebcd23ebb98f75ee1b26fece'
+    "https://api.themoviedb.org/3/movie/popular?api_key=a366c741ebcd23ebb98f75ee1b26fece"
   );
   const res = await data.json();
 
